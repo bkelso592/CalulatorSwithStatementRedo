3b0d9a54721bef346d8a84abaa6dc96a2fc3183a
bk91062 <bk91062@DESKTOP-GBCJOIE>
2024-05-09 16:42:45 -0400

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1, num2;
+    char operation;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    cout << "Enter the operator (+, -, /, *): ";
+    cin >> operation;
+
+    switch (operation) {
+    case '+':
+        cout << num1 << " + " << num2 << " = " << num1 + num2 << endl;
+        break;
+    case '-':
+        cout << num1 << " - " << num2 << " = " << num1 - num2 << endl;
+        break;
+    case '*':
+        cout << num1 << " * " << num2 << " = " << num1 * num2 << endl;
+        break;
+    case '/':
+        if (num2 != 0) {
+            cout << num1 << " / " << num2 << " = " << static_cast<double>(num1) / num2 << endl;
+        }
+        else {
+            cout << "Error: Division by zero is not allowed." << endl;
+        }
+        break;
+    default:
+        cout << "Error: Unsupported operator." << endl;
+    }
+
+    return 0;
+}
